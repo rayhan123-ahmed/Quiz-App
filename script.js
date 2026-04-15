a75634eed8ed89839e7f08eb2b1394b1de253065
Rayhan Ahmed <rmd09181@gmail.com>
2026-04-15 14:31:21 +0600

    Add element selectors for quiz functionality in script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// select all the elements
+const question = document.querySelector("#question");
+const options = document.querySelector(".options");
+const optionBtns = document.querySelectorAll(".option-btn");
+const controls = document.querySelector(".controls");
+const nextBtn = document.querySelector("#nextBtn");
+const scoreBox = document.querySelector(".score-box");
+const score = document.querySelector("#score");
